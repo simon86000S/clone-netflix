07b97608c3c303a7bc11915f917e4e5dffbcb69a
audrain simon <simondu06730@hotmail.fr>
2022-08-29 22:20:24 +0200

    deploy

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -1,7 +1,14 @@
 import React from "react";
 import "../../components/Banner/Banner.scss";
+import PlayArrowIcon from '@mui/icons-material/PlayArrow';
+import InfoIcon from '@mui/icons-material/Info';
 
 const Banner = () => {
+  function truncate(string ,n){
+return string?.length>n ? string.substr(0,n-1)+'......':string
+
+}
+  
   return (
     <div
       className="banner"
@@ -14,19 +21,31 @@ const Banner = () => {
     >
       <div className="banner-content">
         <h1 className="banner__title">Movie Name</h1>
-        <div className="banner__buttons">
-          <button className="banner__button">Play</button>
-          <button className="banner__button">My List</button>
-        </div>
-        <div className="banner_desc">
-        <h1 className="banner_title">this is a decription</h1>
+         <div className="banner_desc">
+        <h1 className="banner_desc_text">
+         {truncate(
+          `this a descrpition this a descrpition this a descrpition this a descrpition
+          this a descrpition this a descrpitionthis a descrpition this a descrpition
+          this a descrpitionthis a descrpitionthis a descrpitionthis a descrpitionthis a descrpition
+          this a descrpitionthis a descrpitionthis a descrpitionthis a descrpition
+          this a descrpitionthis a descrpitionthis a descrpition`
+         ,150)}
+        </h1>
+    
+
       </div>
+        <div className="banner__buttons">
+        <button className="banner__button"><PlayArrowIcon style={{paddingRight:'5px',height:"100px",width:'40px'}}/>Lecture</button>
+          <button className="banner__button"><InfoIcon style={{paddingRight:'5px',height:"100px",width:'40px'}}/>Plus d'Infos</button>
+        </div>
+       
       </div>
       <div className="banner_bottom">
 
       </div>
       
     </div>
-  );
+  )
+         ;
 };
 export default Banner;
